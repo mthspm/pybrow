e87c594dce5c9c54e53ed1707e51619cde3d0367
mthspm <matheusdepmendonca@gmail.com>
2024-12-27 19:24:40 -0300

    ci: add support to read files with hypertext

--- src/core/url.py
+++ src/core/url.py
@@ -1,23 +1,33 @@
 import socket
 import ssl
+import os
 
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https"]
-        if "/" not in url:
-            url = url + "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+        assert self.scheme in ["http", "https", "file"]
+        if self.scheme == "file":
+            self.path = url
+            self.host = None
+            self.port = None
+        else:
+            if "/" not in url:
+                url = url + "/"
+            self.host, url = url.split("/", 1)
+            self.path = "/" + url
+            if self.scheme == "http":
+                self.port = 80
+            elif self.scheme == "https":
+                self.port = 443
+            if ":" in self.host:
+                self.host, port = self.host.split(":", 1)
+                self.port = int(port)
         
     def request(self):
+        if self.scheme == "file":
+            with open(self.path, "r", encoding="utf8") as f:
+                return f.read()
+        
         s = socket.socket(
             family=socket.AF_INET,
             type=socket.SOCK_STREAM,
@@ -76,4 +86,10 @@ def load(url):
   
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
+    
+    TEST_FILE = os.path.join(os.path.dirname(__file__), "..", "assets", "example.html")
+    
+    if len(sys.argv) > 1:
+        load(URL(sys.argv[1]))
+    else:
+        load(URL(f"file://{TEST_FILE}"))
